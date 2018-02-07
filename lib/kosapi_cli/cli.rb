214e6f2505872dd5f4ec73635f4c291ca71b2ce5
Tomas Simacek <simacektomas@volny.cz>
2018-02-07 20:12:45 +0100

    Added global options for CLI. Added XPartial, RSQL and verbose options.

--- lib/kosapi_cli/cli.rb
+++ lib/kosapi_cli/cli.rb
@@ -2,6 +2,20 @@ module KOSapiCLI
   # The main Thor class that will
   # manage all commandline arguments
   class CLI < Thor
+    class_option :verbose,
+                 aliases: '-v',
+                 desc: 'This option enable logging detail information.',
+                 type: :boolean
+
+    class_option :rsql,
+                 aliases: '-r',
+                 desc: 'Specify RSQL command to more complex query.',
+                 type: :string
+
+    class_option :xpartial,
+                 aliases: '-x',
+                 desc: 'Specify XPartial expresion filtering query output.',
+                 type: :string
     # Automatic load of all subcommands
     # in Commands module
     KOSapiCLI::Commands.constants.each do |c|
