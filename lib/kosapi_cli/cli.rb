64c7bc7ffe6857e396a239c572d0d82d53c60131
Tomas Simacek <simacektomas@volny.cz>
2018-02-07 16:03:38 +0100

    Automatic registration of commands in Commands module to main CLI class.

--- lib/kosapi_cli/cli.rb
+++ lib/kosapi_cli/cli.rb
@@ -2,5 +2,15 @@ module KOSapiCLI
   # The main Thor class that will
   # manage all commandline arguments
   class CLI < Thor
+    # Automatic load of all subcommands
+    # in Commands module
+    KOSapiCLI::Commands.constants.each do |c|
+      command = KOSapiCLI::Commands.const_get(c)
+      next unless command.is_a? Class
+      register(command,
+               command.subcommand_name,
+               command.usage,
+               command.description)
+    end
   end
 end
