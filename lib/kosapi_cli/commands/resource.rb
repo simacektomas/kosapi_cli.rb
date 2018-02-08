366a17afaab231a92a79858d6559a76874eb800d
Tomas Simacek <simacektomas@volny.cz>
2018-02-08 22:17:00 +0100

    Excluded resources added to Resource command. It will exclude BaseResource from resources.

--- lib/kosapi_cli/commands/resource.rb
+++ lib/kosapi_cli/commands/resource.rb
@@ -3,11 +3,14 @@ module KOSapiCLI
     # This class will load all subcommands
     # for KOSapi resources
     class Resource < Thor
-
       class_option :rsql,
                    aliases: '-r',
                    desc: 'Specify RSQL command to more complex query.',
                    type: :string
+
+      def self.exclude_commands
+        [:BaseResource]
+      end
 
       def self.subcommand_name
         'resource'
@@ -23,7 +26,7 @@ module KOSapiCLI
 
       KOSapiCLI::Commands::Resources.constants.each do |c|
         subcommand = KOSapiCLI::Commands::Resources.const_get(c)
-        next unless subcommand.is_a? Class
+        next if (!subcommand.is_a? Class) || (exclude_commands.include? c)
         register(subcommand,
                  subcommand.subcommand_name,
                  subcommand.usage,
