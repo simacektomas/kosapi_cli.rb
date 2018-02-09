79019821db29b2e6c6535fcfc65cc6be1e2e114c
Tomas Simacek <simacektomas@volny.cz>
2018-02-09 15:12:13 +0100

    Renamed subcomands method because it overrides Thor method. Took long to catch it.

--- lib/kosapi_cli/commands/base_command.rb
+++ lib/kosapi_cli/commands/base_command.rb
@@ -1,6 +1,6 @@
 module KOSapiCLI
   module Commands
-    # Base class for KOSapiCLI commands,
+    # Base module for KOSapiCLI commands,
     # It provide class methods for
     # name usage and default description
     class BaseCommand < Thor
@@ -8,7 +8,7 @@ module KOSapiCLI
         name.split('::').last.downcase
       end
 
-      def self.subcommands
+      def self.subcommand_names
         sub = []
         public_instance_methods(false).each do |m|
           sub << m.to_s
@@ -17,11 +17,11 @@ module KOSapiCLI
       end
 
       def self.usage
-        "#{command_name} [#{subcommands.join('|')}]"
+        "#{command_name} [#{subcommand_names.join('|')}]"
       end
 
       def self.description
-        "Default desription of #{command_name} command. CHANGE IT!"
+        "Default desription of `#{command_name}` command. CHANGE IT!"
       end
     end
   end
