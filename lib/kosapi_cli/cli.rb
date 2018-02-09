73c66105975fc706bf153cbd5b0980ce61e9bc28
Tomas Simacek <simacektomas@volny.cz>
2018-02-09 15:22:58 +0100

    Return verbose options to root CLI class as it is global option

--- lib/kosapi_cli/cli.rb
+++ lib/kosapi_cli/cli.rb
@@ -2,6 +2,12 @@ module KOSapiCLI
   # The main Thor class that will
   # manage all commandline arguments
   class CLI < Thor
+    class_option :verbose,
+                 aliases: ['-v'],
+                 desc: 'Makes command to print more detailed informations',
+                 type: :boolean,
+                 default: false
+
     def self.excluded_commands
       [:BaseCommand]
     end
@@ -10,6 +16,7 @@ module KOSapiCLI
     KOSapiCLI::Commands.constants.each do |c|
       command = KOSapiCLI::Commands.const_get(c)
       next if (!command.is_a? Class) || (excluded_commands.include? c)
+      desc command.usage, command.description
       register(command,
                command.command_name,
                command.usage,
