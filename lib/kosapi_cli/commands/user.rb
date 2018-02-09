1aac01f36223c94c72d354f4f02d0b5c36a46be0
Tomas Simacek <simacektomas@volny.cz>
2018-02-09 15:21:46 +0100

    User command edited to use BaseCommand. Added descriptions and calling KOSapiCLI api.

--- lib/kosapi_cli/commands/user.rb
+++ lib/kosapi_cli/commands/user.rb
@@ -1,37 +1,51 @@
 module KOSapiCLI
   module Commands
-    #
-    class Login < Thor::Group
-      class_option :username,
-                   aliases: ['-u'],
-                   desc: '',
-                   type: :string
-
-      class_option :password,
-                   aliases: ['-p'],
-                   desc: '',
-                   type: :string
-
-      def self.subcommand_name
-        'login'
-      end
-
-      def self.usage
-          'login [--username=username] [--password=password]'
-      end
-
+    # Class that handles user authentication
+    class User < BaseCommand
       def self.description
-        'Creates .env file in user\'s home directory with access token.'
+        '`user` command handles user login and logout'
       end
 
-      def restore_token
-        @token = KOSapiCLI.loader.restore_token
+      method_option  :username,
+                     aliases: ['-u'],
+                     desc: 'Represents KOSapi client id',
+                     type: :string
+
+      method_option  :password,
+                     aliases: ['-p'],
+                     desc: 'Represents KOSapi client secret',
+                     type: :string
+
+      desc 'login', 'Create .env file in user\'s home if it doesn\'t exist.'
+
+      long_desc <<-LONGDESC
+        `login` command creates .kosapi_cli.env file in user's home
+        directory if it doesn't already exist. If it doesn't or
+        the stored token is not valid user is asked for credentials.
+        Afterwards the new valid token is stored into the file.
+
+        You can specify credentials by two options on commandline.
+        Options are -p|--password and -u|--username. If both options
+        are set, no user input is required.
+
+        `$kosapi_cli user login`
+      LONGDESC
+
+      def login
+        KOSapiCLI.login(options[:username], options[:password])
       end
 
-      def credentials
-      end
+      desc 'logout', 'Remove created .env file if it exists.'
 
-      def store_token
+      long_desc <<-LONGDESC
+        `logout` command search for created .kosapi_cli.env in user's
+        home and delete it.
+
+        `$kosapi_cli user logout`
+      LONGDESC
+
+      def logout
+        KOSapiCLI.logout
       end
     end
   end
