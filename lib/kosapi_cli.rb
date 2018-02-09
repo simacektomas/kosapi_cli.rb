37326c6f261591ffefed0761cc53677eb9401d21
Tomas Simacek <simacektomas@volny.cz>
2018-02-09 15:24:53 +0100

    Adding Authentication module to required files.

--- lib/kosapi_cli.rb
+++ lib/kosapi_cli.rb
@@ -1,10 +1,16 @@
 require 'thor'
 require 'thor/group'
+
 require 'yaml'
+
+require 'kosapi_client'
 
 require 'kosapi_cli/commands'
 
+require 'kosapi_cli/authentication'
 require 'kosapi_cli/configuration'
 require 'kosapi_cli/cli'
+require 'kosapi_cli/kosapi_cli'
+require 'kosapi_cli/kosapi_credentials_loader'
 require 'kosapi_cli/kosapi_token_loader'
 require 'kosapi_cli/version'
