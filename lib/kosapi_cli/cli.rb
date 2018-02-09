1c4f4ebb28133814540c3116b20004ccbc7c8868
Tomas Simacek <simacektomas@volny.cz>
2018-02-09 13:24:47 +0100

    Removed options from CLI class. Added support for excluding some commands as BaseCommand.

--- lib/kosapi_cli/cli.rb
+++ lib/kosapi_cli/cli.rb
@@ -2,27 +2,16 @@ module KOSapiCLI
   # The main Thor class that will
   # manage all commandline arguments
   class CLI < Thor
-    class_option :verbose,
-                 aliases: '-v',
-                 desc: 'This option enable logging detail information.',
-                 type: :boolean
-
-    class_option :rsql,
-                 aliases: '-r',
-                 desc: 'Specify RSQL command to more complex query.',
-                 type: :string
-
-    class_option :xpartial,
-                 aliases: '-x',
-                 desc: 'Specify XPartial expresion filtering query output.',
-                 type: :string
+    def self.excluded_commands
+      [:BaseCommand]
+    end
     # Automatic load of all subcommands
     # in Commands module
     KOSapiCLI::Commands.constants.each do |c|
       command = KOSapiCLI::Commands.const_get(c)
-      next unless command.is_a? Class
+      next if (!command.is_a? Class) || (excluded_commands.include? c)
       register(command,
-               command.subcommand_name,
+               command.command_name,
                command.usage,
                command.description)
     end
