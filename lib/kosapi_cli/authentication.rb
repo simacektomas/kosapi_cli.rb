1140890eea793ff89f1366e008759587a1156e82
Tomas Simacek <simacektomas@volny.cz>
2018-02-11 12:33:47 +0100

    Making smaller granularity of auth methods. To be able to use it for verbose mode.

--- lib/kosapi_cli/authentication.rb
+++ lib/kosapi_cli/authentication.rb
@@ -1,20 +1,6 @@
 module KOSapiCLI
   # Module for KOSapiCLI authentication
   module Authentication
-    def login(id = nil, secret = nil)
-      t = loader.restore_token
-
-      if t
-        configure_client(t, nil, nil)
-      else
-        id ||= creds.ask_client_id
-        secret ||= creds.ask_client_secret
-        configure_client(nil, id, secret)
-      end
-
-      loader.store_token(token)
-    end
-
     def logout
       begin
         File.delete(config.env_file)
@@ -22,6 +8,24 @@ module KOSapiCLI
         return false
       end
       true
+    end
+
+    def initialize_token
+      loaded = loader.restore_token
+      if loaded
+        configure_client(loaded, nil, nil)
+        loader.store_token(token)
+        true
+      else
+        false
+      end
+    end
+
+    def login(id = nil, secret = nil)
+      id ||= creds.ask_client_id
+      secret ||= creds.ask_client_secret
+      configure_client(nil, id, secret)
+      loader.store_token(token)
     end
   end
 end
