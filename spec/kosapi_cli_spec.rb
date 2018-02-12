fd4354aaf5f49f8c4a3738798787a9331532c035
Tomas Simacek <simacektomas@volny.cz>
2018-02-12 19:00:13 +0100

    Test for KOSapiCLI singleton without authentication module.

--- spec/kosapi_cli_spec.rb
+++ spec/kosapi_cli_spec.rb
@@ -1,9 +1,29 @@
-RSpec.describe KosapiCli do
-  it "has a version number" do
-    expect(KosapiCli::VERSION).not_to be nil
+RSpec.describe KOSapiCLI do
+  before do
+    @proxy = KOSapiCLI.proxy
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'has a version number' do
+    expect(KOSapiCLI::VERSION).not_to be nil
+  end
+
+  describe '.proxy' do
+    it 'provide proxy instance via reader' do
+      expect(KOSapiCLI.proxy).to eq @proxy
+    end
+  end
+
+  it 'delegates missing methods to stored proxy instance' do
+    expect(@proxy).to receive(:setup_resource).with('foo')
+    KOSapiCLI.setup_resource('foo')
+  end
+
+  it 'handles missing methods not defined on proxy' do
+    expect { KOSapiCLI.foo }.to raise_error NoMethodError
+  end
+
+  it 'responds to proxy methods' do
+    allow(@proxy).to receive(:foo)
+    expect(KOSapiCLI).to respond_to(:foo)
   end
 end
